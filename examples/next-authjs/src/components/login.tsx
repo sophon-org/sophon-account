2b5ac087785954b9b5aa30a0f21348cc08cd9a3c
Israel Crisanto <israel@sophon.xyz>
2025-05-18 11:29:33 +0100

    feat: updating dynamic libs and implementing siws and next-auth example (#18)
    * feat: updating dynamic libs and implementing siws and next-auth example
    
    * chore: sign message not needed because it doesnt work
    
    * chore: updating frozen lock file

---
+++ examples/next-authjs/src/components/login.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { getCsrfToken, signIn } from "next-auth/react";
+import {
+  SiwsMessage,
+  connect,
+  createEIP1193Provider,
+  getSophonWallet,
+} from "@sophon-labs/account-core";
+import { createWalletClient, custom, Hex } from "viem";
+import { sophon, sophonTestnet } from "viem/chains";
+
+export const LoginButton = () => {
+  const handleConnect = async () => {
+    const networkType = process.env.NETWORK_TYPE as "mainnet" | "testnet";
+    const sophonWallet = await getSophonWallet(networkType);
+    const currentNetwork = networkType === "mainnet" ? sophon : sophonTestnet;
+
+    const connectedWallets = await connect(sophonWallet);
+
+    if (!connectedWallets?.length) {
+      throw new Error("No wallet connected");
+    }
+
+    const provider = createEIP1193Provider(sophonWallet);
+    const walletClient = createWalletClient({
+      account: connectedWallets[0].address as Hex,
+      transport: custom(provider),
+    });
+    const messageBuilder: SiwsMessage = new SiwsMessage({
+      domain: window.location.host,
+      address: connectedWallets[0].address as `0x${string}`,
+      statement: "Hello, world from Sophon and Next-Auth!",
+      chainId: currentNetwork.id,
+      nonce: await getCsrfToken(),
+    });
+
+    const message = messageBuilder.prepareMessage();
+    const signature = await walletClient.signMessage({
+      message,
+    });
+
+    await signIn("credentials", {
+      message,
+      redirect: false,
+      signature,
+      callbackUrl: "/",
+      address: connectedWallets[0].address as `0x${string}`,
+    });
+  };
+
+  return (
+    <button
+      style={{
+        backgroundColor: "purple",
+        padding: 10,
+        borderRadius: 16,
+        color: "white",
+        cursor: "pointer",
+      }}
+      onClick={handleConnect}
+    >
+      Connect
+    </button>
+  );
+};
--- packages/account-core/src/index.ts
+++ packages/account-core/src/index.ts
@@ -1,4 +1,15 @@
 export * from "./config";
 export * from "./wallet";
+export {
+  connect,
+  disconnect,
+  onEvent,
+  switchNetwork,
+  getWalletNetwork,
+  getSupportedEthereumNetworks,
+  getEthereumWallets,
+} from "@dynamic-labs/global-wallet-client/features";
+export { createEIP1193Provider } from "@dynamic-labs/global-wallet-client/ethereum";
 export { isEthereumWallet } from "@dynamic-labs/ethereum";
 export { isZKsyncConnector } from "@dynamic-labs/ethereum-aa-zksync";
+export { SiwsMessage } from "./siws";
