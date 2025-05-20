9b195c65b64f09fb3eb3fbc4f6eb71d4e0bc13db
23Fold <elod@sophon.xyz>
2025-05-21 01:16:12 +0200

    WIP: move components to a standalone package

---
+++ packages/sophon-ui/src/BlueLink.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const BlueLink = ({
+  href,
+  LinkComponent,
+  children,
+}: {
+  href: string;
+  LinkComponent: React.ElementType;
+  children: React.ReactNode;
+}) => {
+  return (
+    <b className="self-stretch relative text-[1rem] [text-decoration:underline] tracking-[-0.01em] leading-[1.5rem] text-royal-blue">
+      <LinkComponent href={href} target="_blank">
+        {children}
+      </LinkComponent>
+    </b>
+  );
+};
+
+export default BlueLink;
