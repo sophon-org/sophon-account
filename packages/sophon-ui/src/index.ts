ab3768685a98e8b2c71aaf5b4059074d76d9c97b
23Fold <elod@sophon.xyz>
2025-05-21 16:40:17 +0200

    removes ui package

--- packages/sophon-ui/src/index.ts
+++
@@ -1,8 +0,0 @@
-import "./globals.css";
-export { default as Button } from "./Button";
-export { default as InfoCard } from "./InfoCard";
-export { default as BlueLink } from "./BlueLink";
-export { default as SendTransactionModal } from "./SendTransactionModal";
-export { default as SignMessageModal } from "./SignMessageModal";
-export { default as TextHighlight } from "./TextHighlight";
-export { default as Background } from "./Background";
