b43c54aca5e3ac5173d7319c3626443f2e93d529
23Fold <elod@sophon.xyz>
2025-05-21 14:08:35 +0200

    WIP: adds icons as svg elemens

---
+++ packages/sophon-ui/src/icons/LogOut.tsx
@@ -0,0 +1,10 @@
+import type { SVGProps } from "react";
+const SvgIcLogOut = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" {...props}>
+    <path
+      fill="#122B5C"
+      d="M11.25 20.25a.75.75 0 0 1-.75.75h-6a.75.75 0 0 1-.75-.75V3.75A.75.75 0 0 1 4.5 3h6a.75.75 0 1 1 0 1.5H5.25v15h5.25a.75.75 0 0 1 .75.75m10.28-8.78-3.75-3.75a.75.75 0 0 0-1.28.53v3h-6a.75.75 0 1 0 0 1.5h6v3a.75.75 0 0 0 1.28.53l3.75-3.75a.747.747 0 0 0 0-1.06"
+    />
+  </svg>
+);
+export default SvgIcLogOut;
---
+++ packages/sophon-ui/src/icons/Vercel.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from "react";
+const SvgVercel = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 1155 1000" {...props}>
+    <path fill="#fff" d="m577.3 0 577.4 1000H0z" />
+  </svg>
+);
+export default SvgVercel;
