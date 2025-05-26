d96e07b6ddfacd378c76c3f2634b4be860bc4c2a
Ramon "9Tails" Canales <emaildoramon@gmail.com>
2025-05-26 11:39:31 +0100

    Feat/20250524 - Adding SNS support and Avatar generation (#23)
    * chore: updating zksync-sso and removing unused files from another branch
    
    * chore: adding sns capabilities and avatar generation
    
    * chore: adjusting avatar generator
    
    * chore: finished SNS functions for v0
    
    * chore: improving function readability and code organization
    
    * chore: cleaning up test console log, and adding helper to check if address is sophon account

--- packages/account-core/src/snsHelper.ts
+++ packages/account-core/src/snsHelper.ts
@@ -1,40 +1,70 @@
-import { mainnet } from "viem/chains";
-import { Address, createPublicClient, http } from "viem";
-import { sophonTestnet } from "viem/chains";
+import { Address, createPublicClient, http, isAddress, namehash, pad, toHex } from "viem";
+import { sophonTestnet, sophon } from "viem/chains";
 import { snsRegistryAbi } from "./abis/SNSRegistryAbi";
 
-const SNS_REGISTRY_ADDRESS = "0x5a1f427df38564e764f3e400137252989e92cc96";
+const SNS_REGISTRY_ADDRESS = "0x3951eDF6D554FcB0327b4D5218Dd1B8BD90B456e";
 
 export const resolveName = async (
   name: string,
-  testnet?: boolean
+  testnet?: boolean,
+  rpcUrl?: string,
 ): Promise<Address> => {
   const client = createPublicClient({
-    chain: testnet ? sophonTestnet : mainnet,
-    transport: http(),
+    chain: testnet ? sophonTestnet : sophon,
+    transport: http(rpcUrl),
   });
+
+  if (isAddress(name)) {
+    throw new Error("An address is not a valid name");
+  }
+
+  // Clean up in case it was provided with the .soph.id suffix
+  const _name = `${name.toLowerCase().replace(".soph.id", "")}.soph.id`;
+
+  const hash = namehash(_name);
+
   const resolved = await client.readContract({
     address: SNS_REGISTRY_ADDRESS,
     abi: snsRegistryAbi,
     functionName: "addr",
-    args: [name],
+    args: [hash],
   });
   return resolved as Address;
 };
 
 export const resolveAddress = async (
   address: string,
-  testnet?: boolean
+  testnet?: boolean,
+  rpcUrl?: string,
 ): Promise<string> => {
   const client = createPublicClient({
-    chain: testnet ? sophonTestnet : mainnet,
-    transport: http(),
+    chain: testnet ? sophonTestnet : sophon,
+    transport: http(rpcUrl),
   });
-  const resolved = await client.readContract({
+
+  if (!isAddress(address)) {
+    throw new Error("You provided an invalid address");
+  }
+
+  const tokenId = await client.readContract({
     address: SNS_REGISTRY_ADDRESS,
     abi: snsRegistryAbi,
-    functionName: "ownedDomains",
-    args: [address],
+    functionName: "tokenOfOwnerByIndex",
+    args: [address, 0],
   });
-  return resolved[0];
+
+  if (!tokenId) {
+    throw new Error("No name found for this address");
+  }
+
+  const nameHash = pad(toHex(tokenId as bigint), { size: 32 });
+  
+  const name = await client.readContract({
+    address: SNS_REGISTRY_ADDRESS,
+    abi: snsRegistryAbi,
+    functionName: "name",
+    args: [nameHash],
+  });
+
+  return `${name}.soph.id` as string;
 };
